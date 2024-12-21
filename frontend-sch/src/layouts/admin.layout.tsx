c4e61a15eae67dfd8f5d97345e30d790c9982560
masood <masoodacheampong@gmail.com>
2024-12-21 23:58:20 +0000

    change:setup dashboard navbar.

--- frontend-sch/src/layouts/admin.layout.tsx
+++ frontend-sch/src/layouts/admin.layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { AppSidebar } from "@/components/shared/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import DashboardNavbar from "@/components/shared/navbar/dashboard-navbar";
 export default function AdminLayout() {
   return (
     <>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
+          <DashboardNavbar />
           <Outlet />
         </SidebarInset>
       </SidebarProvider>
