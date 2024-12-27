95126509a6d0336768cceef6fe969f4880ad016a
masood <masoodacheampong@gmail.com>
2024-12-27 16:03:56 +0000

    completed working on edit subject

--- frontend-sch/src/features/admin/pages/subjects/edit/edit-subject-form.tsx
+++ frontend-sch/src/features/admin/pages/subjects/edit/edit-subject-form.tsx
@@ -1,5 +1,85 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ButtonLoader } from "@/components/shared/loaders/button-loader";
+import { useEffect } from "react";
+import { useUpdateSubject } from "@/features/admin/services/subjects/queries";
 
-export default function EditSubjectForm() {
-  return <div>EditSubjectForm</div>;
+const formSchema = z.object({
+  name: z.string().optional(),
+});
+
+export default function EditSubjectForm({
+  subjectData,
+}: {
+  subjectData: {
+    id: string;
+    name: string;
+  };
+}) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  useEffect(() => {
+    if (subjectData) {
+      form.reset({
+        name: subjectData.name,
+      });
+    }
+  }, [subjectData, form]);
+  const { mutateAsync: updateSubject, isLoading: updatingSubjectLoader } =
+    useUpdateSubject();
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!subjectData) return;
+    const data = {
+      id: subjectData?.id.toString(),
+      name: values.name,
+    };
+    try {
+      await updateSubject(data);
+    } catch (error) {
+      console.error("Form submission error", error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Name - Introduction to Science"
+                  type="name"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Modify subject name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <ButtonLoader type="submit" isLoading={updatingSubjectLoader}>
+          Update Subject
+        </ButtonLoader>
+      </form>
+    </Form>
+  );
 }
--- frontend-sch/src/features/admin/pages/subjects/edit/edit-subject.tsx
+++ frontend-sch/src/features/admin/pages/subjects/edit/edit-subject.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import Header from "@/features/admin/components/header/header";
+import { useParams } from "react-router-dom";
+import EditSubjectForm from "./edit-subject-form";
 
 export default function EditSubject() {
-  return <div>EditSubject</div>;
+  const { id } = useParams<{ id: string }>();
+  // const {data:subjectData} =
+  const data = {
+    id: "1",
+    name: "Mathematics",
+  };
+  return (
+    <section className="w-full max-w-6xl mx-auto py-4 space-y-10">
+      {/* Header */}
+      <Header
+        title="Edit Subject"
+        description="Update/Modify contents for this subject"
+        buttonInfo={{ title: "Go Back", link: "/admin/subjects" }}
+      />
+      {/* Form */}
+      <div>
+        <EditSubjectForm subjectData={data} />
+      </div>
+    </section>
+  );
 }
